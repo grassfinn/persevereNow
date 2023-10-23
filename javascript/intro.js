40157753ca4a7c918fe4233d19666df1d80aed6f
grassfinn <theharper@live.com>
2023-10-23 09:23:50 -0400

    JS Variables

---
+++ javascript/intro.js
@@ -0,0 +1,13 @@
+const firstName = 'Andrew'
+const lastName = 'Harper'
+let age = 29;
+
+console.log(firstName) 
+console.log(lastName)
+console.log(age)
+
+// Hello Andrew Harper you are 29 years old!
+
+console.log('Hello' + ' ' + firstName + ' ' + lastName + ' you are ' + age + ' years old!')
+
+console.log(`Hello ${firstName} ${lastName} you are ${age} years old!`)

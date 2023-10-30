f6a0221ee33ad37a7841cfa7b9129ca05c5429f6
grassfinn <theharper@live.com>
2023-10-30 18:51:26 -0400

    Guess the Number Game

--- javascript/activities/function/randomNumberGuessingGame.js
+++ javascript/activities/function/randomNumberGuessingGame.js
@@ -1,67 +1,56 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let attempts = 0;
 const randomNumber = Math.floor(Math.random() * 10) + 1;
+const guessBtn = document.getElementById('guessBtn');
 
 function hint() {
-  let userInput = prompt('Pick a number');
-  if (!userInput) {
-    return;
+  // Elements
+  const titleElement = document.getElementById('title');
+  const randomNumberElement = document.getElementById('randomNumber');
+  const attemptsElement = document.getElementById('attempts');
+  const previousGuessElement = document.getElementById('previous');
+  const hintElement = document.getElementById('hint');
+  let userInput = document.getElementById('userInput');
+  if (userInput.value === "") {
+    previousGuessElement.textContent = 'Please Enter a Number';
+    return
   }
+  attempts++;
+
   if (attempts === 10) {
-    alert('You Lose!');
+    hintElement.textContent = 'You Lose!';
+    attempts = 0;
     return;
   }
 
-  if (userInput == randomNumber) {
-    alert('You Win!');
-    alert(`It took you ${attempts} attempts`);
-
+  if (userInput.value === randomNumber) {
+    randomNumberElement.textContent = randomNumber;
+    titleElement.textContent = `It took you ${attempts} attempts`;
+    hintElement.textContent = 'You Win!';
     return;
   }
-  if (userInput < randomNumber) {
-    alert('Higher');
-    attempts++;
+  if (userInput.value < randomNumber) {
+    attemptsElement.textContent = attempts;
+    previousGuessElement.textContent = userInput.value;
+    hintElement.textContent = 'Higher';
+    userInput.value = '';
+    userInput.focus;
   }
-  if (userInput > randomNumber) {
-    alert('Lower');
-    attempts++;
+  if (userInput.value > randomNumber) {
+    attemptsElement.textContent = attempts;
+    previousGuessElement.textContent = userInput.value;
+    hintElement.textContent = 'Lower';
+    userInput.value = '';
+    userInput.focus;
   }
-  hint();
 }
-hint();
+
+function handleSubmit(e) {
+  if (e.key === 'Enter') {
+    hint();
+  }
+}
+
+userInput.addEventListener('keypress', handleSubmit);
+guessBtn.addEventListener('click', hint);
+// hint()
+// window.onload = hint //we want our html to load first before we run script
